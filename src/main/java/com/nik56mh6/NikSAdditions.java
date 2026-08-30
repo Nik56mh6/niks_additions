1c014d65bdb9af501a08773015f92bf1a2c0bf54
nik56mh6 <nicolasfodor1@gmail.com>
2026-08-30 23:00:26 +0200

    1.0.1
    Immediate_respawn has been forced to true by default
    
    22:59

--- src/main/java/com/nik56mh6/NikSAdditions.java
+++ src/main/java/com/nik56mh6/NikSAdditions.java
@@ -1,7 +1,9 @@
 package com.nik56mh6;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.resources.Identifier;
+import net.minecraft.server.MinecraftServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.nik56mh6.ItemScripts.*;
@@ -15,13 +17,24 @@ public class NikSAdditions implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 		ModItems.initialize();
 		ModSounds.register();
+		ServerTickEvents.START_SERVER_TICK.register(this::onServerTick);
 
 		LOGGER.info("Nik's additions have loaded correctly!");
+	}
+
+	private void onServerTick(MinecraftServer server) {
+		boolean isImmediateRespawnRuleSet = false;
+
+		boolean immediateRespawnRule = server.getGameRules().get(net.minecraft.world.level.gamerules.GameRules.IMMEDIATE_RESPAWN);
+		if(!immediateRespawnRule == isImmediateRespawnRuleSet) return; {
+			server.getGameRules().set(net.minecraft.world. level.gamerules.GameRules.IMMEDIATE_RESPAWN, true, server);
+		}
 	}
 
 	public static Identifier id(String path) {
