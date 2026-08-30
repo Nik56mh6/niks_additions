2ecc6560582bf25c029853c1f9db2c92799041d3
nik56mh6 <nicolasfodor1@gmail.com>
2026-08-30 03:14:57 +0200

    meh

--- src/main/java/com/nik56mh6/ModSounds.java
+++ src/main/java/com/nik56mh6/ModSounds.java
@@ -1,11 +1,8 @@
 package com.nik56mh6;
-import com.nik56mh6.ModItems;
-import com.nik56mh6.ItemScripts.Gourdofreturn;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.sounds.SoundEvent;
-import com.nik56mh6.ItemScripts.Gourdofreturnfinish;
 
 public class ModSounds {
     public static final SoundEvent GOURD_OF_RETURN = SoundEvent.createVariableRangeEvent(
